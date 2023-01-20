acc0e7752d1ea0ef46e48387a849d3bf2ed73c8f
jhar1994 <107641636+jhar1994@users.noreply.github.com>
2023-01-20 17:22:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -131,10 +131,9 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port= process.env.PORT;
-if (port == null || port == ""){
-  port =3000;
-}
+let port= process.env.PORT || 3000;
+
+
 app.listen(port, function() {
   console.log('Server started on ${PORT}');
 });
